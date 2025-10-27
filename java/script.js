e937f1cafdc9f6f02ed3e8d2724ce0f779ffabd8
marioliso9 <140639804+marioliso9@users.noreply.github.com>
2025-10-27 09:50:15 -0300

    0000

--- java/script.js
+++ java/script.js
@@ -1,48 +1,50 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas"); 
-const caixaAlternativas = document.querySelector(".caixa-aternativas"); 
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-aternativas");
 const caixaResultados = document.querySelector(".caixa-resultados");
 const textoResultado = document.querySelector(".texto-resultado");
-const botaoJogarNovamente= document.querySelector(".novamente-botao"); 
-const telaInicial = document.querySelector(".tela-inicial"); 
+const botaoJogarNovamente= document.querySelector(".novamente-botao");
+const telaInicial = document.querySelector(".tela-inicial");
 const botaoIniciar = document.querySelector(".iniciar-botao");
 
-let atual= 0;
+let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
-botaoInicial.addEventListener('click,iniciaJogo');
+botaoIniciar.addEventListener('click,iniciaJogo');
 function iniciaJogo (){
     atual = 0;
     historiaFinal = "";
     telaInicial.style.display = 'none';
     caixaPerguntas.classList.remove("mostrar");
-    caixaAlternativas.classlist.remove("mostrar");
-    caixaResultados.classlist.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
     mostraPergunta();
 }
+
 function mostraPergunta (){
     if (atual >= perguntas.length){
         mostraResultado();
         return;
     }
-perguntaAtual = perguntas[atual];
-caixaPerguntas.textContent = perguntaAtual.enunciado;
-caixaAlternativas.textContent = "";
- mostraAlternativa();
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
+
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas);{
+    for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
-}
-function mostraAfirmacoes(){
-    for(const afirmacoes of perguntaAtual.afirmacoes){
-
     }
-}
+    function mostraAfirmacoes(){
+        for(const afirmacoes of perguntaAtual.afirmacoes){
+
+        }
+    }
 }
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
@@ -51,7 +53,7 @@ function respostaSelecionada(opcaoSelecionada){
         atual = opcaoSelecionada.proxima;
     }else{
         mostraResultado();
-        return; 
+        return;
     }
     mostraPergunta();
 }
@@ -62,7 +64,7 @@ function mostraResultado(){
     caixaResultados.classList.add("mostrar");
     botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
-function jogaNovamente(){
+function jogaNovamente{
     atual = 0;
     historiaFinal = "";
     caixaResultados.classList.remove("mostrar");
@@ -70,7 +72,7 @@ function jogaNovamente(){
 }
 function substituiNome(){
     for (const pergunta of perguntas){
-        pergunta.enunciado = pergunta.eninciado.replace(/você/g, nome);
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
     }
 }
 substituiNome();
